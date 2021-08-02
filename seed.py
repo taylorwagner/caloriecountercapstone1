974dc4d255e7f45d1b5bfea08412c23c5f26329e
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-02 11:59:48 -0500

    sample profile data and 404.html page

--- seed.py
+++ seed.py
@@ -25,3 +25,15 @@ db.session.add.all([u1, u2, u3, u4])
 
 # Commit
 db.session.commit()
+
+# Add sample profiles
+p1 = Profile(user_id=553131, first_name="Santana", last_name="Porter", city="Houston", state="TX", gender=False, dob="05/31/2000", reason="Meet people on a health journey and share my process with others", goal_cal=1800)
+p2 = Profile(user_id=771010, goal_cal=1300)
+p3 = Profile(user_id=660101, first_name="Megan", city="Crockett", state="TX", gender=True, dob="06/01/1981", goal_cal=1000)
+p4 = Profile(user_id=440202, first_name="Paul", last_name="Stewart", city="Los Angeles", state="CA", gender=False, goal_cal=3000)
+
+# Add new objects to sesion, so they'll persist
+db.session.add.all([p1, p2, p3, p4])
+
+# Commit
+db.session.commit()
