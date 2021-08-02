940b9df862c01313f13e25c625b45e5f5d6b6e48
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-02 14:12:24 -0500

    change datarequired to inputrequired for familiarity

--- forms.py
+++ forms.py
@@ -2,12 +2,18 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.fields.core import IntegerField
+from wtforms.validators import InputRequired, Length
 
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired(message="Unique username is required."), Length(max=25)])
-    password = PasswordField('Password', validators=[DataRequired(message="Must create a password between 8-30 characters."), Length(min=8, max=30)])
-    email = StringField('E-mail', validators=DataRequired("Unique e-mail address is required."))
+    username = StringField('Username', validators=[InputRequired(message="Unique username is required."), Length(max=25)])
+    password = PasswordField('Password', validators=[InputRequired(message="Must create a password between 8-30 characters."), Length(min=8, max=30)])
+    email = StringField('E-mail', validators=[InputRequired("Unique e-mail address is required.")])
+
+class UserProfileForm(FlaskForm):
+    """Form for creating user's profile information."""
+
+    goal_cal = IntegerField("Daily Caloric Goal", validators=[InputRequired()])
