3abbaeca26e24f8c4fd611f856400c6573b3e497
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-02 14:14:12 -0500

    can't do min/max on db.text

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.Text(min=8, max=30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
     email = db.Column(db.Text, nullable=False, unique=True)
 
     comments = db.relationship('Comment')
