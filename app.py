3ba6f6ad6e754feca4aa932034308a352b11700f
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-02 10:41:49 -0500

    set up app

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Calorie Counter Flask App."""
+import os
+from flask import Flask
+import flask_sqlalchemy
+from models import db, connect_db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgres:///calcount'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+
+connect_db(app)
