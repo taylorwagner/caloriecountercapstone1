48672f0f77650cd8b6ed5dceb42bd0723eea8969
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-04 10:14:00 -0500

    update schema and seed for model changes

--- seed.py
+++ seed.py
@@ -15,10 +15,10 @@ Follow.query.delete()
 Comment.query.delete()
 
 # Add sample users
-u1 = User(id=553131, username="sanrpor", password="password", email="sanrpor@test.com")
-u2 = User(id=771010, username="leopeezy3", password="testpassword", email="lpp@gmail.com")
-u3 = User(id=660101, username="meglporter", password="testpwpw", email="fake@email.com")
-u4 = User(id=440202, username="pauliefbaby", password="samplepassword", email="sports@nfl.com")
+u1 = User(id=553131, username="sanrpor", password="password", email="sanrpor@test.com", goal_cal=1800)
+u2 = User(id=771010, username="leopeezy3", password="testpassword", email="lpp@gmail.com", goal_cal=1300)
+u3 = User(id=660101, username="meglporter", password="testpwpw", email="fake@email.com", goal_cal=1000)
+u4 = User(id=440202, username="pauliefbaby", password="samplepassword", email="sports@nfl.com", goal_cal=3000)
 
 # Add new objects to sesion, so they'll persist
 db.session.add.all([u1, u2, u3, u4])
@@ -27,10 +27,10 @@ db.session.add.all([u1, u2, u3, u4])
 db.session.commit()
 
 # Add sample profiles
-p1 = Profile(user_id=553131, first_name="Santana", last_name="Porter", city="Houston", state="TX", gender=False, dob="05/31/2000", reason="Meet people on a health journey and share my process with others", goal_cal=1800)
-p2 = Profile(user_id=771010, goal_cal=1300)
-p3 = Profile(user_id=660101, first_name="Megan", city="Crockett", state="TX", gender=True, dob="06/01/1981", goal_cal=1000)
-p4 = Profile(user_id=440202, first_name="Paul", last_name="Stewart", city="Los Angeles", state="CA", gender=False, goal_cal=3000)
+p1 = Profile(user_id=553131, first_name="Santana", last_name="Porter", city="Houston", state="TX", gender=False, dob="05/31/2000", reason="Meet people on a health journey and share my process with others")
+p2 = Profile(user_id=771010)
+p3 = Profile(user_id=660101, first_name="Megan", city="Crockett", state="TX", gender=True, dob="06/01/1981")
+p4 = Profile(user_id=440202, first_name="Paul", last_name="Stewart", city="Los Angeles", state="CA", gender=False)
 
 # Add new objects to sesion, so they'll persist
 db.session.add.all([p1, p2, p3, p4])
