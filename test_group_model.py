e6940431b9c2b5943e765ff173f6291a9d57d7e9
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-07 13:46:51 -0500

    basic group model test

--- test_group_model.py
+++ test_group_model.py
@@ -15,3 +15,37 @@ db.create_all()
 
 class GroupModelTestCase(TestCase):
     """Test model for group."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
+
+        self.testuserid = 2012
+        testuser = User.signup("testuser", "fakepassword", "emailemail@email.com", 2012, "Bronx", "NY")
+        testuser.id = self.testuserid
+        db.session.commit()
+
+        self.testuser = User.query.get(self.testuserid)
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    def test_group_model(self):
+        """Does basic model work?"""
+        testgroup = Group(name="#farmers", description="For people who live on the farm or love to farm!")
+
+        testgroup2 = Group(name="#nodescription")
+
+        db.session.add_all([testgroup, testgroup2])
+        db.session.commit()
+
+
+        self.assertEqual(testgroup.name, "#farmers")
+        self.assertEqual(testgroup.description, "For people who live on the farm or love to farm!")
+        self.assertEqual(testgroup2.name, "#nodescription")
+        self.assertIsNone(testgroup2.description, None)
