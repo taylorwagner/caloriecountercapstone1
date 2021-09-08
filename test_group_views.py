cf16413fcbbdb5c996f9d1e160897137d99997ce
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-09-07 22:35:17 -0500

    group show testing

--- test_group_views.py
+++ test_group_views.py
@@ -44,8 +44,53 @@ class GroupViewTestCase(TestCase):
                 sess[CURR_USER_KEY] = self.testuser.id
 
             testgroup = Group.query.get(999999999)
+            testgroup2 = Group.query.get(999999998)
 
             res = c.get("/groups")
             self.assertIn("testgroup", str(res.data))
             self.assertIn("testgroup2", str(res.data))
+            self.assertIn(testgroup2.name, str(res.data))
             self.assertNotIn("nogroup", str(res.data))
+
+    # def test_new_group(self):
+    #     """Test that a new group can be added with the form."""
+    #     with self.client as c:
+    #         with c.session_transaction() as sess:
+    #             sess[CURR_USER_KEY] = self.testuser.id
+
+    #         res = c.post("/groups/new", data={"name": "TESTING GROUP!!!!"}, follow_redirects=True)
+
+    #         self.assertEqual(res.status_code, 302)
+
+    #         group = Group.query.one()
+    #         self.assertEqual(group.name, "TESTING GROUP!!!!")
+
+    def test_show_group(self):
+        """Test to detect that when an authorized user clicks on a valid group page, the group shows along with description (if valid) and included users (if valid)."""
+        testgroupdesc = Group(id=999999997, name="testtesttestgroup", description="test group including a description")
+
+        db.session.add(testgroupdesc)
+        db.session.commit()
+
+        testusergroup = UserGroup(id=99999, group_id=999999997, user_id=self.testuser.id)
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            group = Group.query.get(999999997)
+
+            res = c.get(f"/groups/{group.id}")
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(group.name, str(res.data)) 
+
+    def test_invalid_show_group(self):
+        """Test that 404 page/message will kick in for an invalid group id"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            res = c.get("/groups/9ujh8y689")
+
+            self.assertEqual(res.status_code, 404)
